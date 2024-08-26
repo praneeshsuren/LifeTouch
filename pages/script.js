28fdce42749f693d721d88856f3658f86efd5633
praneeshsuren <praneeshsuren@gmail.com>
2024-08-26 12:59:47 +0530

    "feat: complete front end for login page and sidebar component"

--- pages/script.js
+++ pages/script.js
@@ -1,6 +1,19 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const body = document.querySelector('body');
+      sidebar = document.querySelector('.sidebar');
+      toggle = document.querySelector('.toggle');
+      modeSwitch = document.querySelector('.toggle-switch');
+      modeText = document.querySelector('.mode-text');
 
-menu.onclick = () => {
-    navbar.classList.toggle('open');
-}
+      toggle.addEventListener('click', () => {
+        sidebar.classList.toggle('close');
+      });
+
+      modeSwitch.addEventListener('click', () => {
+        body.classList.toggle('dark');
+
+        if(body.classList.contains('dark')) {
+          modeText.innerText = 'Light Mode';
+        }else{
+            modeText.innerText = 'Dark Mode';
+        }
+      });
